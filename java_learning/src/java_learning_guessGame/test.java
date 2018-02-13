e9b75818515d07a38266ef4535d9eb94d18ead82
shengyang gu <gushengyang@sina.cn>
2018-02-13 17:04:18 +0800

    1

---
+++ java_learning/src/java_learning_guessGame/test.java
@@ -0,0 +1,20 @@
+package java_learning_guessGame;
+import java.io.File;  
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;  
+import java.io.BufferedWriter;  
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;  
+
+
+public class test{
+	public static void main(String[] args){
+		GuessGame game= new GuessGame();
+		game.startGame();
+	}	
+}	
